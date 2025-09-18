f8a214c454141137a6ec8630a4a56bfef4ce8b5a
momin210103 <momincse13@gmail.com>
2025-09-18 15:46:24 +0600

    Delete position node

--- LinkedList/DeleteNode.cpp
+++ LinkedList/DeleteNode.cpp
@@ -47,6 +47,31 @@ Node* DeleteTail(Node* head){
     tmp->next = nullptr;
     return head;
 }
+Node* DeletePosition(Node* head,int pos){
+    if(head == NULL){
+        return head;
+    }
+    if(pos==1){
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;
+        return head;
+    }
+    Node* tmp = head;
+    int cnt =0;
+    Node* prev = NULL;
+    while(tmp!=NULL){
+        cnt++;
+        if(cnt == pos){
+            prev->next = prev->next->next;
+            delete tmp;
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    return head;
+}
 Node* Print(Node* head){
     while(head){
         cout<<head->data<<"->";
@@ -58,6 +83,8 @@ int main(){
     Node* head = Input();
     head = DeleteHead(head);
     head = DeleteTail(head);
+    Print(head);
+    head = DeletePosition(head,3);
     Print(head);
     return 0;
 }
